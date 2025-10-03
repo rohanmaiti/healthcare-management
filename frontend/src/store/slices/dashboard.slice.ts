395161c7aac810e95b7191a9d27963ce622bc31f
rohanmaiti <maitirohan2019@gmail.com>
2025-10-03 18:07:27 +0530

    dashboard slice updated

--- frontend/src/store/slices/dashboard.slice.ts
+++ frontend/src/store/slices/dashboard.slice.ts
@@ -1,24 +1,29 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 import axiosInstance from "../../lib/axios";
 import type { ApiError } from "../../utils/interfaces";
 
-// interface Hospitaladmindetails {
-    
-// }
-
-export const login = createAsyncThunk<
-//   Hospitaladmindetails,
-  { rejectValue: string }
->("/hospital-admin/dashboard/details", async (data, { rejectWithValue }) => {
-  try {
-    const res = await axiosInstance.post("/hospital-admin/v1/dashboard/details", data);
-    return res.data;
-  } catch (error) {
-    const apiError = error as ApiError;
-    console.log(apiError);
-    return rejectWithValue(
-      apiError?.response?.data?.message || apiError?.message || "Login failed"
-    );
+export const login = createAsyncThunk<{ rejectValue: string }>(
+  "/hospital-admin/dashboard/details",
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = await axiosInstance.get(
+        "/hospital-admin/v1/dashboard/details"
+      );
+      return res.data;
+    } catch (error) {
+      const apiError = error as ApiError;
+      console.log(apiError);
+      return rejectWithValue(
+        apiError?.response?.data?.message || apiError?.message || "Login failed"
+      );
+    }
   }
-});
+);
+
+// const hospital_admin_slice = createSlice({
+//   name: "hospital_admin_slice",
+//   initialState: {
+//     details: null,
+//   } as any,
+// });
